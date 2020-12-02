7645edd6f724094e14ec6ac431afc87752d33637
Matthias Larsen <mlarsen@gammamedia.org>
2020-12-02 14:36:23 +0100

    wip
    Signed-off-by: Matthias Larsen <mlarsen@gammamedia.org>

--- src/routes.php
+++ src/routes.php
@@ -1,14 +1,15 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Signifly\Kubernetes\Kubernetes;
 
 if (config('kubernetes.healthchecks')) {
-    Route::get('/healthz/liveness', function ($request) {
+    Route::get('/healthz/liveness', function (Request $request) {
         return Kubernetes::httpLiveness($request);
     });
 
-    Route::get('/healthz/readiness', function ($request) {
+    Route::get('/healthz/readiness', function (Request $request) {
         return Kubernetes::httpReadiness($request);
     });
 }
